24788b211e86e5820b00d43292345287269a1549
nata13pr <nata13pr@gmail.com>
2026-04-16 12:24:01 +0300

    add validation partly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,34 @@
-import Form from "next/form";
-import {getMeals, saveMeal} from "@/server-actions/serverActions";
+import {useForm} from "react-hook-form";
+import {UserFormData} from "@/lib/schema";
+import {zodResolver} from "@hookform/resolvers/zod";
 
 export default async function Home() {
+    const {register, handleSubmit, formState: {errors}} = useForm<UserFormData>(
+        {
+            resolver: zodResolver
+        }
+    )
 
-    const mealsArray = await getMeals()
+    const onSubmit = async (data: UserFormData) => {
+        const res = await fetch('api/users', {
+            method: 'POST',
+            body: JSON.stringify(data),
+        })
 
+        const json = await res.json()
+        console.log(json)
+    }
     return (
-        <div>
-            <Form action={saveMeal}>
-                <input type="text" name='title'/>
-                <button>send</button>
-            </Form>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <div>
-                {
-                    mealsArray.map(meal => <div key={meal.id}>{meal.title}</div>)
-                }
+                <input {...register('name')} placeholder='Name'/>
             </div>
-        </div>
+            <div>
+                <input {...register('email')} placeholder='Email'/>
+            </div>
+            <div>
+                <input {...register('password')} placeholder='Passsword'/>
+            </div>
+        </form>
     );
 }
